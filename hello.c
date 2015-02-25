943929a3dd0622c4f48f0fe0a2dd41762844a1af
arwen <74552925@qq.com>
2015-02-25 23:09:24 +0800

    f

---
+++ hello.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: hello.c
+	> Author: Arwen
+	> Mail:745529725@qq.com 
+	> Created Time: Wed 25 Feb 2015 11:07:42 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	printf("OK\n");
+	return 0;
+}
