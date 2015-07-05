93f5a0eb8fea00727e9901c457ecb4cabfe64f4a
arwen <74552925@qq.com>
2015-07-05 19:09:25 +0800

    11

---
+++ Shell/userstats.sh
@@ -0,0 +1,11 @@
+#########################################################################
+# File Name: userstats.sh
+# Author: Arwen
+# mail: 745529725@qq.com
+# Created Time: Sat 04 Jul 2015 11:00:43 PM CST
+#########################################################################
+#!/bin/bash
+echo"Hello, $LOGNAME"
+echo"Current date is $date"
+echo"User is $'who i am'"
+echo"Current directory $pwd"
